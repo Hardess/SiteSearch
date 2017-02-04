4d4eaf5504cebbdfe58d839219dd8dbd2c69fc4e
Hardess <624010189@qq.com>
2017-02-04 16:01:44 +0800

    完善压缩文件读取
    新增了对rar文件的解压功能，并区分rar和zip压缩文件的解压方式

--- src/SiteSearch/FileTools.cs
+++ src/SiteSearch/FileTools.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 using NPOI;
 
 namespace SiteSearch
@@ -22,16 +24,16 @@ namespace SiteSearch
             List<Model> list = new List<Model>();
             switch (FileType)
             {
-                case "doc":
-                case "docx":
+                case ".doc":
+                case ".docx":
                     list.AddRange(GetWordFile(FilePath));
                     break;
-                case "xls":
-                case "xlsx":
+                case ".xls":
+                case ".xlsx":
                     list.AddRange(GetExcelFile(FilePath));
                     break;
-                case "rar":
-                case "zip":
+                case ".rar":
+                case ".zip":
                     list.AddRange(GetCompressFile(FilePath));
                     break;
                 default:
@@ -50,14 +52,22 @@ namespace SiteSearch
             List<Model> list = new List<Model>();
             //压缩文件解压后的路径
             string ToPath = @"D:\SiteSearchData";
+            FileInfo F = new FileInfo(FilePath);
             if (!System.IO.Directory.Exists(ToPath))
             {
                 System.IO.Directory.CreateDirectory(ToPath);//不存在就创建目录 
             }
-            using (ZipArchive Archive = ZipFile.Open(FilePath, ZipArchiveMode.Update))
+            if (F.Extension == ".zip")
             {
-                //解压文件
-                Archive.ExtractToDirectory(ToPath);
+                using (ZipArchive Archive = ZipFile.Open(FilePath, ZipArchiveMode.Update))
+                {
+                    //解压文件
+                    Archive.ExtractToDirectory(ToPath);
+                }
+            }
+            else
+            {
+                DeCompressRar(FilePath, ToPath);
             }
             if (Directory.GetFileSystemEntries(ToPath).Length > 0)
             {
@@ -71,12 +81,12 @@ namespace SiteSearch
                         //根据压缩包内文件的类型生成实体集合
                         switch(fi.Extension)
                         {
-                            case "doc":
-                            case "docx":
+                            case ".doc":
+                            case ".docx":
                                 list.AddRange(GetWordFile(FilePath));
                                 break;
-                            case "xls":
-                            case "xlsx":
+                            case ".xls":
+                            case ".xlsx":
                                 list.AddRange(GetExcelFile(FilePath));
                                 break;
                             default:
@@ -87,7 +97,49 @@ namespace SiteSearch
             }
             return list;
         }
+        /// <summary>
+        /// 将格式为rar的压缩文件解压到指定的目录
+        /// </summary>
+        /// <param name="rarFileName">要解压rar文件的路径</param>
+        /// <param name="saveDir">解压后要保存到的目录</param>
+        public static void DeCompressRar(string rarFilePath, string ToPath)
+        {
+            string regKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe";
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(regKey);
+            string winrarPath = registryKey.GetValue("").ToString();
+            registryKey.Close();
+            string winrarDir = System.IO.Path.GetDirectoryName(winrarPath);
+            String commandOptions = string.Format("x {0} {1} -y", rarFilePath, ToPath);
 
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = System.IO.Path.Combine(winrarDir, "rar.exe");
+            processStartInfo.Arguments = commandOptions;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+        }
+        /// <summary>
+        /// 本机是否安装winrar程序
+        /// </summary>
+        /// <returns></returns>
+        public static string ExistsWinRar()
+        {
+            string result = string.Empty;
+
+            string key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe";
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(key);
+            if (registryKey != null)
+            {
+                result = registryKey.GetValue("").ToString();
+            }
+            registryKey.Close();
+
+            return result;
+        }
         private static List<Model> GetExcelFile(string FilePath)
         {
             //TODO:为Excel文件内容生成实体集合
