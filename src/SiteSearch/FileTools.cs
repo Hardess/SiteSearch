96bda62251ab71e00e561db9ce6ad379ccb8881b
Hardess <624010189@qq.com>
2017-02-04 14:07:38 +0800

    update tools
    暂时完善解压文件方法。

--- src/SiteSearch/FileTools.cs
+++ src/SiteSearch/FileTools.cs
@@ -12,7 +12,7 @@ namespace SiteSearch
     public class FileTools
     {
         /// <summary>
-        /// 根据文件类型获取实体
+        /// 根据文件类型获取实体集合
         /// </summary>
         /// <param name="FilePath">附件路径</param>
         /// <param name="FileType">附件后缀名</param>
@@ -42,10 +42,13 @@ namespace SiteSearch
 
         private static List<Model> GetCompressFile(string FilePath)
         {
-            throw new NotImplementedException();
+            //TODO:解压压缩文件并为其中的文件内容生成实体集合
+           return  LoadData(FilePath);
         }
-        private void LoadData(string FilePath)
+        private static List<Model> LoadData(string FilePath)
         {
+            List<Model> list = new List<Model>();
+            //压缩文件解压后的路径
             string ToPath = @"D:\SiteSearchData";
             if (!System.IO.Directory.Exists(ToPath))
             {
@@ -53,7 +56,7 @@ namespace SiteSearch
             }
             using (ZipArchive Archive = ZipFile.Open(FilePath, ZipArchiveMode.Update))
             {
-
+                //解压文件
                 Archive.ExtractToDirectory(ToPath);
             }
             if (Directory.GetFileSystemEntries(ToPath).Length > 0)
@@ -61,18 +64,39 @@ namespace SiteSearch
                 //遍历文件夹中所有文件  
                 foreach (string file in Directory.GetFiles(ToPath))
                 {
-
+                    //文件是否存在 
+                    if (File.Exists(file))
+                    {
+                        FileInfo fi = new FileInfo(file);
+                        //根据压缩包内文件的类型生成实体集合
+                        switch(fi.Extension)
+                        {
+                            case "doc":
+                            case "docx":
+                                list.AddRange(GetWordFile(FilePath));
+                                break;
+                            case "xls":
+                            case "xlsx":
+                                list.AddRange(GetExcelFile(FilePath));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
                 }
             }
+            return list;
         }
 
         private static List<Model> GetExcelFile(string FilePath)
         {
+            //TODO:为Excel文件内容生成实体集合
             throw new NotImplementedException();
         }
 
         private static List<Model> GetWordFile(string FilePath)
         {
+            //TODO:为Word文件内容生成实体集合
             throw new NotImplementedException();
         }
     }
