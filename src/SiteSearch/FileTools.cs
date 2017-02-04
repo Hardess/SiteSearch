b8aa8d632a110e93e1263c1fcb6768657f2dd938
Hardess <624010189@qq.com>
2017-02-04 13:42:52 +0800

    初具雏形

--- src/SiteSearch/FileTools.cs
+++ src/SiteSearch/FileTools.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +9,7 @@ using NPOI;
 
 namespace SiteSearch
 {
-   public class FileTools
+    public class FileTools
     {
         /// <summary>
         /// 根据文件类型获取实体
@@ -17,25 +19,51 @@ namespace SiteSearch
         /// <returns>创建索引时所需要的listmodel</returns>
         private static List<Model> GetModelListByFileType(string FilePath, string FileType)
         {
+            List<Model> list = new List<Model>();
             switch (FileType)
             {
                 case "doc":
                 case "docx":
-                    return GetWordFile(FilePath);
+                    list.AddRange(GetWordFile(FilePath));
+                    break;
                 case "xls":
                 case "xlsx":
-                    return GetExcelFile(FilePath);
+                    list.AddRange(GetExcelFile(FilePath));
+                    break;
                 case "rar":
                 case "zip":
-                    return GetCompressFile(FilePath);
-
-                default: return new List<Model>();
+                    list.AddRange(GetCompressFile(FilePath));
+                    break;
+                default:
+                    break;
             }
+            return list;
         }
 
         private static List<Model> GetCompressFile(string FilePath)
         {
             throw new NotImplementedException();
+        }
+        private void LoadData(string FilePath)
+        {
+            string ToPath = @"D:\SiteSearchData";
+            if (!System.IO.Directory.Exists(ToPath))
+            {
+                System.IO.Directory.CreateDirectory(ToPath);//不存在就创建目录 
+            }
+            using (ZipArchive Archive = ZipFile.Open(FilePath, ZipArchiveMode.Update))
+            {
+
+                Archive.ExtractToDirectory(ToPath);
+            }
+            if (Directory.GetFileSystemEntries(ToPath).Length > 0)
+            {
+                //遍历文件夹中所有文件  
+                foreach (string file in Directory.GetFiles(ToPath))
+                {
+
+                }
+            }
         }
 
         private static List<Model> GetExcelFile(string FilePath)
